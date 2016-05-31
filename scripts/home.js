7553b59b94bbacd128a2ab51f4fa5ce029d21a57
Aaron Crawfis <acrawfis@nd.edu>
2016-05-30 21:03:49 -0400

    Database download done
    Still needs filters

---
+++ scripts/home.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var allPostsSection = document.getElementById('all-posts-list');
+
+/**
+ * Creates a post element.
+ */
+function createPostElement(title, date, time, description, category) {
+
+var eventID = '';
+
+switch(category){
+	case 'Sports':
+		eventID = '<div id="event1">';
+		break;
+	case 'Art':
+		eventID = '<div id="event2">';
+		break;
+	case 'Music':
+		eventID = '<div id="event3">';
+		break;
+	case 'ATV':
+		eventID = '<div id="event5">';
+		break;
+	case 'Camp':
+		eventID = '<div id="event4">';
+		break;
+	default:
+		eventID = '<div id="event">';
+		break;
+}
+		
+
+  var html =
+      eventID+
+      '<h2 class="category"></h2>' +
+      '<h3 class="title"></h3>' +
+      '<h4 class="date"></h4>' +
+      '<!-- <h4>at</h4>' +
+      '<h4 class="time"></h4> -->' +
+      '<h4 class="location"></h4>' +
+      '<p class="description"></p>' +
+      '</div>';
+
+  // Create the DOM element from the HTML.
+  var div = document.createElement('div');
+  div.innerHTML = html;
+  var postElement = div.firstChild;
+
+  // Set values.
+  postElement.getElementsByClassName('category')[0].innerText = category;
+  postElement.getElementsByClassName('title')[0].innerText = title;
+  postElement.getElementsByClassName('date')[0].innerText = date + ' at ' + time;
+  //postElement.getElementsByClassName('time')[0].innerText = time;
+  //postElement.getElementsByClassName('location')[0].innerText = location;
+  postElement.getElementsByClassName('description')[0].innerText = description;
+
+  return postElement;
+}
+
+/**
+ * Starts listening for new posts and populates posts lists.
+ */
+function startDatabaseQueries() {
+  var allPostsRef = firebase.database().ref('posts/');
+
+  var fetchPosts = function(postsRef, sectionElement) {
+    postsRef.on('child_added', function(data) {
+      var containerElement = sectionElement.getElementsByClassName('posts-container')[0];
+      containerElement.insertBefore(
+          createPostElement(data.val().event, data.val().date, data.val().time, data.val().description, data.val().category),
+          containerElement.firstChild);
+    });
+  };
+  
+  fetchPosts(allPostsRef, allPostsSection);
+}
+
+// Bindings on load.
+window.addEventListener('load', function() {
+	// Listen for auth state changes
+    startDatabaseQueries(); 
+    allPostsSection.style.display = 'block';
+}, false);
--- scripts/main.js
+++ scripts/main.js
@@ -7,30 +7,24 @@ var dateInput = document.getElementById('new-date');
 var ageInput = document.getElementById('new-date');
 var timeInput = document.getElementById('new-time');
 var descriptionInput = document.getElementById('new-description');
+var categoryInput = document.getElementById('new-category');
 var postButton = document.getElementById('post-button');
-
-/*
-var recentPostsSection = document.getElementById('recent-posts-list');
-var userPostsSection = document.getElementById('user-posts-list');
-var topUserPostsSection = document.getElementById('top-user-posts-list');
-var recentMenuButton = document.getElementById('menu-recent');
-var myPostsMenuButton = document.getElementById('menu-my-posts');
-var myTopPostsMenuButton = document.getElementById('menu-my-top-posts');
-*/
 
 /**
  * Saves a new post to the Firebase DB.
  */
 // [START write_fan_out]
-function writeNewPost(firstName, lastName, event, date, age, time, description) {
+function writeNewPost(firstName, lastName, event, date, age, time, description, category) {
   // A post entry.
   var postData = {
     firstName: firstName,
     lastName: lastName,
+    event: event,
     date: date,
     age: age,
     time: time,
-    description, description
+    description, description,
+    category: category
   };
 
   // Get a key for a new Post.
@@ -39,92 +33,10 @@ function writeNewPost(firstName, lastName, event, date, age, time, description) 
   // Write the new post's data simultaneously in the posts list and the user's post list.
   var updates = {};
   updates['/posts/' + newPostKey] = postData;
-  //updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-
+  
   return firebase.database().ref().update(updates);
 }
 // [END write_fan_out]
-
-/**
- * Creates a post element.
- */
-function createPostElement(postId, title, text, author) {
-  var uid = firebase.auth().currentUser.uid;
-
-  var html =
-      '<div class="post mdl-cell mdl-cell--12-col ' +
-                  'mdl-cell--6-col-tablet mdl-cell--4-col-desktop mdl-grid mdl-grid--no-spacing">' +
-        '<div class="mdl-card mdl-shadow--2dp">' +
-          '<div class="mdl-card__title mdl-color--light-blue-600 mdl-color-text--white">' +
-            '<h4 class="mdl-card__title-text"></h4>' +
-          '</div>' +
-          '<div class="header">' +
-            '<div>' +
-              '<div class="avatar"></div>' +
-              '<div class="username mdl-color-text--black"></div>' +
-            '</div>' +
-          '</div>' +
-          '<span class="star">' +
-            '<div class="not-starred material-icons">star_border</div>' +
-            '<div class="starred material-icons">star</div>' +
-            '<div class="star-count">0</div>' +
-          '</span>' +
-          '<div class="text"></div>' +
-          '<div class="comments-container"></div>' +
-          '<form class="add-comment" action="#">' +
-            '<div class="mdl-textfield mdl-js-textfield">' +
-              '<input class="mdl-textfield__input new-comment" type="text">' +
-              '<label class="mdl-textfield__label">Comment...</label>' +
-            '</div>' +
-          '</form>' +
-        '</div>' +
-      '</div>';
-
-  // Create the DOM element from the HTML.
-  var div = document.createElement('div');
-  div.innerHTML = html;
-  var postElement = div.firstChild;
-  componentHandler.upgradeElements(postElement.getElementsByClassName('mdl-textfield')[0]);
-
-  var addCommentForm = postElement.getElementsByClassName('add-comment')[0];
-  var commentInput = postElement.getElementsByClassName('new-comment')[0];
-  var star = postElement.getElementsByClassName('starred')[0];
-  var unStar = postElement.getElementsByClassName('not-starred')[0];
-
-  // Set values.
-  postElement.getElementsByClassName('text')[0].innerText = text;
-  postElement.getElementsByClassName('mdl-card__title-text')[0].innerText = title;
-  postElement.getElementsByClassName('username')[0].innerText = author;
-
-  return postElement;
-}
-
-/**
- * Starts listening for new posts and populates posts lists.
- */
-function startDatabaseQueries() {
-  // [START my_top_posts_query]
-  var myUserId = firebase.auth().currentUser.uid;
-  var topUserPostsRef = firebase.database().ref('user-posts/' + myUserId).orderByChild('starCount');
-  // [END my_top_posts_query]
-  // [START recent_posts_query]
-  var recentPostsRef = firebase.database().ref('posts').limitToLast(100);
-  // [END recent_posts_query]
-  var userPostsRef = firebase.database().ref('user-posts/' + myUserId);
-
-  var fetchPosts = function(postsRef, sectionElement) {
-    postsRef.on('child_added', function(data) {
-      var containerElement = sectionElement.getElementsByClassName('posts-container')[0];
-      containerElement.insertBefore(
-          createPostElement(data.key, data.val().title, data.val().body, data.val().author),
-          containerElement.firstChild);
-    });
-  };
-
-  fetchPosts(topUserPostsRef, topUserPostsSection);
-  fetchPosts(recentPostsRef, recentPostsSection);
-  fetchPosts(userPostsRef, userPostsSection);
-}
 
 // Bindings on load.
 window.addEventListener('load', function() {
@@ -134,11 +46,11 @@ window.addEventListener('load', function() {
     e.preventDefault();
     if (fNameInput.value && lNameInput.value && eventInput.value && dateInput.value && ageInput.value && timeInput.value && descriptionInput.value) {
         // [START_EXCLUDE]
-        writeNewPost(fNameInput.value, lNameInput.value, eventInput.value, dateInput.value, ageInput.value, timeInput.value, descriptionInput.value).then(function() {
-              myPostsMenuButton.click();
+        writeNewPost(fNameInput.value, lNameInput.value, eventInput.value, dateInput.value, ageInput.value, timeInput.value, descriptionInput.value, categoryInput.value).then(function() {
+              console.log("Uploaded");
+              window.location.replace('./home.html');
             });
         // [END_EXCLUDE]
     };
   }
- //recentMenuButton.onclick();
 }, false);
